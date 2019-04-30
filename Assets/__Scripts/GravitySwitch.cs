cff4aa92ecced22aae403df6a5df0120a57eed1f
Ben Goldstein <bg3mail@gmail.com>
2019-04-30 11:32:26 -0400

    Gravity Switching is a map I'm gonna make

---
+++ Assets/__Scripts/GravitySwitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySwitch : MonoBehaviour {
+
+	void OnCollisionEnter(Collision coll) {
+        if(coll.collider.tag == "Player") {
+            Physics.gravity = new Vector3(-9.8f, 0, 0);
+        }
+    }
+}
