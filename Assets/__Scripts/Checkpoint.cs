7b48c446e4ece0dacb2e018d2440db9596398e9f
penryoa <oliviapenry@gmail.com>
2019-05-16 00:23:50 -0400

    finished the checkpoints. not sure what to do with two players though

---
+++ Assets/__Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            FinishLine parent = GetComponentInParent<FinishLine>();
+            parent.Checkpoint_triggered(gameObject);
+        }   
+    }
+}
--- Assets/__Scripts/FinishLine.cs
+++ Assets/__Scripts/FinishLine.cs
@@ -10,25 +10,29 @@ public class FinishLine : MonoBehaviour {
 
     [Header("Depends on the race")]
     public int total_laps = 3;
+    public int num_checkpoints = 3;
     public Text lapCounter;
     public Text timeCounter;
-    bool startedRace;
-    bool hasWon;
+    bool startedRace; //refers to state of the game
+    bool hasWon; // refers to state of the game
     float elapsedTime;
+    public List<GameObject> cp = new List<GameObject>();
+    protected List<int> passed_checkpoints = new List<int>();
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
         startedRace = false;
         hasWon = false;
         elapsedTime = 0.0f;
+        for (int i = 0; i < num_checkpoints; i++)
+        {
+            passed_checkpoints.Insert(i,0);
+        }
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        if (num_laps > total_laps)
-        {
-            hasWon = true;
-        }
+        if (num_laps > total_laps) hasWon = true;
+
+        Completed_lap();
 
         if (!hasWon)
         {
@@ -38,7 +42,6 @@ public class FinishLine : MonoBehaviour {
             int sec_i = (int)elapsedTime % 60;
             string sec = sec_i.ToString();
             if (sec_i < 10) sec = "0" + sec;
-
 
             timeCounter.text = min + ":" + sec;
         }
@@ -56,7 +59,44 @@ public class FinishLine : MonoBehaviour {
         if (coll.tag == "Player")
         {
             if (!startedRace) startedRace = true;
-            else num_laps++;
+            else
+            {
+                if (Completed_lap())
+                {
+                    num_laps++;
+                    Reset_lap();
+                }
+            }
+        }
+    }
+    
+
+    public void Checkpoint_triggered(GameObject go)
+    {
+        int i = cp.IndexOf(go);
+        if (i > -1)
+        {
+            //print(go.name);
+            //print(i);
+            passed_checkpoints.Insert(i, 1);
+        }
+    }
+
+    private bool Completed_lap()
+    {
+        bool completed = true;
+        for (int c  = 0; c < num_checkpoints; c++)
+        {
+            if (passed_checkpoints[c] == 0) completed = false;
+        }
+        return completed;
+    }
+
+    private void Reset_lap()
+    {
+        for (int i = 0; i < num_checkpoints; i++)
+        {
+            passed_checkpoints.Insert(i,0);
         }
     }
 }
