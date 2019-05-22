49a2b004e3b151f575bd17f67f3330dd55b08acb
Jocelynn <jcheese034@gmail.com>
2019-05-22 00:19:53 -0400

    added all powerup stuff

---
+++ Assets/__Scripts/Powerup2_Text.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Powerup2_Text : MonoBehaviour {
+
+	public Text powerupText2 = null;
+	public Player_alt player2;
+
+	// Update is called once per frame
+	void Update () {
+		int item2 = player2.returnPlayer2Item ();
+		if (item2 == 1) {
+			powerupText2.text = "Powerup available: SPEED BOOST";
+		} else if (item2 == 2) {
+			powerupText2.text = "Powerup available: TIME WARP";
+		} else if (item2 == 3) {
+			powerupText2.text = "Powerup available: ASTEROID";
+		} else if (item2 == 4) {
+			powerupText2.text = "Powerup available: NEWTON REGULATOR";
+		} else if (item2 == 5) {
+			powerupText2.text = "Powerup available: HIJACK";
+		} else {
+			powerupText2.text = "Powerup available: NONE";
+		}
+	}
+}
---
+++ Assets/__Scripts/Powerup_Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_Box : MonoBehaviour {
+
+	void OnTriggerEnter(Collider coll)
+	{
+		//See if a player hit the powerup box
+		GameObject collidedWith = coll.gameObject;
+
+		if (collidedWith.tag == "Player" || collidedWith.tag == "Player2") {
+
+			//if the powerup box gets collided with, make it disappear for 10 seconds
+			GetComponent<MeshRenderer>().enabled = false;
+			GetComponent<Collider> ().enabled = false;
+			//GameObject.active = false;
+			Invoke ("Reappear", 5f);
+		}
+
+	}
+
+	void Reappear(){
+		GetComponent<MeshRenderer>().enabled = true;
+		GetComponent<Collider> ().enabled = true;
+		//GameObject.active = true;
+	}
+}
